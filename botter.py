c1eb842c58e81c05981bff32fa8f8f0520581086
lenanya <lenashimizu@gmx.net>
2025-05-01 01:41:28 +0200

    formatting

--- botter.py
+++ botter.py
@@ -187,6 +187,6 @@ async def ip(ctx):
 async def pwd(ctx):
   with open("cwd", "r") as f:
     cwd = f.read()
-  await ctx.send(sprintf("lena is in %", cwd))
+  await ctx.send(sprintf("lena is in `%`", cwd))
 
 bot.run(token)
